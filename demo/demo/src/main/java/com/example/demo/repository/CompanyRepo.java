be9d33210d8d2b2a7a3542829be59c8d1b300e4c
vinnny1904 <116699064+vinnny1904@users.noreply.github.com>
2025-02-11 17:42:16 +0800

    Delete demo/demo/src/main/java/com/example/demo directory

--- demo/demo/src/main/java/com/example/demo/repository/CompanyRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepo extends JpaRepository<Company, Integer> {
-}
